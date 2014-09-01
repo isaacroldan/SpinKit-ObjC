eee87f67bb30254066cc556d881eefb88713db49
isaacroldan <isaac.roldan@gmail.com>
2014-09-01 11:39:39 +0200

    Customize bounce animation

--- SpinKit/RTSpinKitView.m
+++ SpinKit/RTSpinKitView.m
@@ -117,34 +117,34 @@ static CATransform3D RTSpinKit3DRotationWithPerspective(CGFloat perspective,
         else if (style == RTSpinKitViewStyleBounce) {
             NSTimeInterval beginTime = CACurrentMediaTime();
             
-            for (NSInteger i=0; i < 2; i+=1) {
+            for (NSInteger i=0; i < 3; i+=1) {
                 CALayer *circle = [CALayer layer];
                 circle.frame = CGRectInset(self.bounds, 2.0, 2.0);
                 circle.backgroundColor = color.CGColor;
                 circle.anchorPoint = CGPointMake(0.5, 0.5);
-                circle.opacity = 0.6;
+                circle.opacity = 0.15*(i+1);
                 circle.cornerRadius = CGRectGetHeight(circle.bounds) * 0.5;
                 circle.transform = CATransform3DMakeScale(0.0, 0.0, 0.0);
-                
+
                 CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
                 anim.removedOnCompletion = NO;
                 anim.repeatCount = HUGE_VALF;
-                anim.duration = 2.0;
+                anim.duration = 3.0;
                 anim.beginTime = beginTime - (1.0 * i);
                 anim.keyTimes = @[@(0.0), @(0.5), @(1.0)];
-                
+
                 anim.timingFunctions = @[
-                                         [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
-                                         [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
-                                         [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]
-                                         ];
-                
+                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],
+                    [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]
+                ];
+
                 anim.values = @[
-                                [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)],
-                                [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
-                                [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)]
-                                ];
-                
+                    [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)],
+                    [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 0.0)],
+                    [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.0, 0.0, 0.0)]
+                ];
+
                 [self.layer addSublayer:circle];
                 [circle addAnimation:anim forKey:@"spinkit-anim"];
             }
